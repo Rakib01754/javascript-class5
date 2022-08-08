58249a87806d82fc5ef75e5426f51fbb650bb5fe
Rakib01754 <rakibul.freelancer567@gmail.com>
2022-08-08 15:25:06 +0600

    even odd added

---
+++ evenodd.js
@@ -0,0 +1,24 @@
+// function checkEvenOdd(number) {
+//     if (number % 2 == 0) {
+//         return true;
+//     }
+//     else {
+//         return false;
+//     }
+// }
+
+// let theNumber = checkEvenOdd(98);
+// console.log(theNumber);
+
+
+function checkEvenOdd(number) {
+    if (number % 2 == 0) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
+let theNumber = checkEvenOdd(117);
+console.log(theNumber);
